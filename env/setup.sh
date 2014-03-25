0d27b836521d129d0e755880378a30e8e2726084
Rob Hermans <r.j.hermans@gmail.com>
2014-03-25 22:50:51 +0100

    ch2 finished

--- env/setup.sh
+++
@@ -1,8 +0,0 @@
-# Make sure we get the latest suorces
-apt-get update
-
-# Prevent installers from asking too much shit
-export DEBIAN_FRONTEND=noninteractive
-apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
-apt-get -y install vim
-apt-get -y install fail2ban
